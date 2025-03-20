2a5843519c67d433e3fbd160846230bc4ffeb332
xKuroiUsagix <povoroznikvitalii@gmail.com>
2025-03-20 15:25:50 +0200

    create database tables

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+    books = relationship('Book', back_populates='authors')
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+
+    author = relationship('Author', back_populates='books')
